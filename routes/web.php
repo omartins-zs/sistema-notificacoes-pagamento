824525dad04d40e529d7fd98a5678f26752092d8
omartins-zs <gabrielmatheus209@gmail.com>
2025-12-24 16:17:16 -0300

    🛣️ Adicionar rotas web do sistema

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Http\Controllers\CobrancaController;
+use App\Http\Controllers\NotificacaoController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('cobrancas.index');
 });
+
+Route::get('/cobrancas', [CobrancaController::class, 'index'])->name('cobrancas.index');
+Route::get('/cobrancas/create', [CobrancaController::class, 'create'])->name('cobrancas.create');
+Route::post('/cobrancas', [CobrancaController::class, 'store'])->name('cobrancas.store');
+Route::post('/cobrancas/{cobranca}/notificar', [NotificacaoController::class, 'store'])->name('notificacoes.store');
+Route::get('/notificacoes', [NotificacaoController::class, 'index'])->name('notificacoes.index');
